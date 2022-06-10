892b79ded15ff15b6dfc10bbb240addd9f03fbfa
Yoo hyuntak <yhyuntak@gmail.com>
2022-06-10 15:59:01 +0900

    220610

--- common/util.py
+++ common/util.py
@@ -138,7 +138,7 @@ def clip_grads(grads, max_norm):
     rate = max_norm / (total_norm + 1e-6)
     if rate < 1:
         for grad in grads:
-            grad = rate*grad
+            grad *= rate
 
 
 def to_cpu(x):
@@ -154,3 +154,33 @@ def to_gpu(x):
         return x
     return cupy.asarray(x)
 
+
+def eval_perplexity(model, corpus, batch_size=10, time_size=35):
+    print('퍼플렉서티 평가 중 ...')
+    corpus_size = len(corpus)
+    total_loss, loss_cnt = 0, 0
+    max_iters = (corpus_size - 1) // (batch_size * time_size)
+    jump = (corpus_size - 1) // batch_size
+
+    for iters in range(max_iters):
+        xs = np.zeros((batch_size, time_size), dtype=np.int32)
+        ts = np.zeros((batch_size, time_size), dtype=np.int32)
+        time_offset = iters * time_size
+        offsets = [time_offset + (i * jump) for i in range(batch_size)]
+        for t in range(time_size):
+            for i, offset in enumerate(offsets):
+                xs[i, t] = corpus[(offset + t) % corpus_size]
+                ts[i, t] = corpus[(offset + t + 1) % corpus_size]
+
+        try:
+            loss = model.forward(xs, ts, train_flg=False)
+        except TypeError:
+            loss = model.forward(xs, ts)
+        total_loss += loss
+
+        sys.stdout.write('\r%d / %d' % (iters, max_iters))
+        sys.stdout.flush()
+
+    print('')
+    ppl = np.exp(total_loss / max_iters)
+    return ppl
